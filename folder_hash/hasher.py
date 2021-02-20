8a88044b7658fb34171d6ed49da8955b3d21f7d9
krazykirby99999 <krazykirby99999@gmail.com>
2021-02-20 10:03:23 -0600

    Add get_hash functionality

--- folder_hash/hasher.py
+++ folder_hash/hasher.py
@@ -1,13 +1,29 @@
 import hashlib
+import os
+
+BUF_SIZE = 65536
+
 
 class Hasher:
     def __init__(self, folder_path, hash_algo):
         self.folder_path = folder_path
         self.hash_algo = hash_algo
-        self.folder_path = None
+        self.folder_hash = self.get_hash()
 
     def get_hash(self):
-        pass
+        try:
+            hasher = hashlib.new(self.hash_algo)
+        except Exception as e:
+            raise ValueError("Unsupported hash algorithm chosen. " + e)
+
+        for root, dirs, files in os.walk(self.folder_path):
+            for file in files:
+                with open(os.path.join(root, file), "rb", BUF_SIZE) as f:
+                    file_data = f.read()
+                    hasher.update(file_data)
+            for dir in dirs:
+                hasher.update(dir.encode("utf-8"))
+        return hasher.hexdigest()
 
     def set_hash_algo(self):
         pass
