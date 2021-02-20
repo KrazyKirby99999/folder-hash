5ee3e5a8798b171ff533a4e62229aff64c8599d8
krazykirby99999 <krazykirby99999@gmail.com>
2021-02-20 08:30:05 -0600

    Create hasher module with placeholders

---
+++ folder_hash/hasher.py
@@ -0,0 +1,16 @@
+import hashlib
+
+class Hasher:
+    def __init__(self, folder_path, hash_algo):
+        self.folder_path = folder_path
+        self.hash_algo = hash_algo
+        self.folder_path = None
+
+    def get_hash(self):
+        pass
+
+    def set_hash_algo(self):
+        pass
+
+    def set_folder_path(self):
+        pass
